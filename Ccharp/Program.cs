541094930f44b24f71f7c01edad830d42857d483
Gwenitora <gwenitora.gardu74@gmail.com>
2024-03-20 20:04:43 +0100

    Feature: try to make async

--- Ccharp/Program.cs
+++ Ccharp/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Drawing;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
 
-class Example
+class Progam
 {
-    public static void Main()
+    private async static void GameLoop()
     {
         ascii asc = new ascii();
+        Preload(asc);
         while (true)
         {
             var _h = Console.WindowHeight;
@@ -13,7 +16,35 @@ class Example
             Console.SetCursorPosition(0, 0);
             Console.Write(asc.loadImg("cat-example.png"));
         }
+    }
 
+    public static void Main()
+    {
         Colored.resetColor();
+        GameLoop();
+        Colored.resetColor();
+    }
+
+    private async static void Preload(ascii asc)
+    {
+        List<string> imgToLoad = new List<string>()
+        {
+            "cat-example.png"
+        };
+
+        int minPix = 100 * 100;
+
+        foreach (string img in imgToLoad)
+        {
+            for (int i = 1; i < 375; i++)
+            {
+                for (int j = 1; j < 750; j++)
+                {
+                    if (i * j < minPix) continue;
+
+                    asc.loadImg(img, i, j);
+                }
+            }
+        }
     }
 }
--- Ccharp/ascii.cs
+++ Ccharp/ascii.cs
@@ -6,10 +6,11 @@ using System.Text;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 public class ascii
 {
-    private string chars = " .-+:=*#%@";
+    private string chars = " ";
 
     public ascii()
     {
@@ -45,20 +46,14 @@ public class ascii
             dist = _dist;
             saved = i;
         }
-        if (moy < 255f / 2f - 0.5f)
-        {
-            return Colored.getStrictColor(allColor._PersoCol, (int)r, (int)g, (int)b, 0, 0, 0) + getChar((int)moy);
-        }
         return Colored.getStrictColor((int)allColor._PersoCol + (int)allColor.TextBlack, 0, 0, 0, (int)r, (int)g, (int)b) + getChar((int)moy);
     }
 
-    public List<int> resizeImg(int height, int width)
+    public List<int> resizeImg(int height, int width, int h, int w)
     {
         width *= 2;
         var ratio = ((float)height) / ((float)width);
 
-        var h = Console.WindowHeight;
-        var w = Console.WindowWidth;
         var r = ((float)h) / ((float)w);
 
         int endH;
@@ -78,39 +73,78 @@ public class ascii
 
     private List<int> getColorZone(Bitmap img, int x, int y, int h)
     {
-        int w = 2 * h;
+        int w = h / 2;
 
         float r = 0, g = 0, b = 0, l = 0;
 
-        for (int i = 0; i < h; i++)
+        List<int> list = new List<int>(6) {x, y, x+w, y+h, 0, 0};
+
+        for (int i = 0; i < Math.Min(h, 10); i++)
         {
-            for (int j = 0; j < w; j++)
+            for (int j = 0; j < Math.Min(w, 20); j++)
             {
-                if (x + j >= img.Width || y + i >= img.Height) continue;
-                var col = img.GetPixel(x + j, y + i);
+                if (x + j >= img.Height || y + i >= img.Width) continue;
+                var col = img.GetPixel(y + i, x + j);
                 r += col.R;
                 g += col.G;
                 b += col.B;
                 l++;
+
+                if (x + j > list[4])
+                {
+                    list[4] = x + j;
+                }
+                if (y + i > list[5])
+                {
+                    list[5] = y + i;
+                }
             }
         }
 
         return new List<int>(3) {(int)(r / l), (int)(g / l), (int)(b / l)};
     }
 
-    public string loadImg(string imgPath)
+    private bool checkDir(string imgPath, int h, int w)
     {
-        string res = "";
-        var img = new Bitmap("../../../img/" + imgPath);
+        var Path = "../../../img/ascii/";
 
-        var size = resizeImg(img.Height, img.Width);
+        if (!Directory.Exists(Path))
+        {
+            Directory.CreateDirectory(Path + imgPath + "/");
+        }
+        if (!Directory.Exists(Path + imgPath + "/"))
+        {
+            Directory.CreateDirectory(Path + imgPath + "/");
+        }
+        if (!File.Exists(Path + imgPath + "/" + h + "x" + w + ".txt"))
+        {
+            File.CreateText(Path + imgPath + "/" + h + "x" + w + ".txt").Close();
+            return false;
+        }
+        return true;
+    }
+
+    public string loadImg(string imgPath, int h, int w)
+    {
+        w = Math.Min(w, 750);
+        w = Math.Max(w, 1);
+        h = Math.Min(h, 375);
+        h = Math.Max(h, 1);
+        var Path = "../../../img/";
+        string res = "";
+
+        if (checkDir(imgPath, h, w))
+        {
+            return File.ReadAllText(Path + "ascii/" + imgPath + "/" + h + "x" + w + ".txt");
+        }
+
+        var img = new Bitmap(Path + imgPath);
+
+        var size = resizeImg(img.Height, img.Width, h, w);
         var height = size[0];
         var width = size[1];
         var offsetH = size[2];
         var offsetW = size[3];
-
-        var h = Console.WindowHeight;
-        var w = Console.WindowWidth;
 
         Console.CursorVisible = false;
 
@@ -130,16 +164,24 @@ public class ascii
                     }
                     var col = getColorZone(
                         img,
-                        (int)(((float)i - (float)offsetH) / (float)height) * img.Height,
-                        (int)(((float)j - (float)offsetW) / (float)width) * img.Width,
+                        (int)(((float)i - (float)offsetH) / (float)height * (float)img.Height),
+                        (int)(((float)j - (float)offsetW) / (float)width * (float)img.Width),
                         (int)((float)img.Height / (float)h)
                     );
                     string t = genChar(col[0], col[1], col[2]);
 
                     res += t;
                 }
+                res += "\n";
             }
         }
+
+        File.WriteAllText(Path + "ascii/" + imgPath + "/" + h + "x" + w + ".txt", res);
+
         return res;
+    }
+    public string loadImg(string imgPath)
+    {
+        return loadImg(imgPath, Console.WindowHeight, Console.WindowWidth);
     }
 }
