9fce4c4dcda964e336179b50b0e4b4436fce0277
roidescrabes <117281009+enzo-pulzatto@users.noreply.github.com>
2024-03-20 15:03:58 +0100

    debut de game object

---
+++ Ccharp/gameobject/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+public abstract class GameObject
+{
+    public float PosX { get; protected set; }
+    public float PosY { get; protected set; }
+    public int IdShape { get; protected set; }
+    public int IdMap { get; protected set; }
+
+    public GameObject(float posX, float posY, int idShape, int idMap)
+    {
+        PosX = posX;
+        PosY = posY;
+        IdShape = idShape;
+        IdMap = idMap;
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+//menuobject: il faut savoir a quoi il ressemble , savoir ou ils seront rangé donc quel liste ils integres , position sur la map , 
