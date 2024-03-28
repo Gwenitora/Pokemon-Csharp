8d113abef15578b4143834d88d812087e43ed374
roidescrabes <117281009+enzo-pulzatto@users.noreply.github.com>
2024-03-28 08:42:58 +0100

    playr / dresseur update

---
+++ Ccharp/gameobject/Items/Dresseur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Dresseur : GameObject
+{
+    public string Name { get; private set; }
+    public List<Chakimon> ChakimonEquipe { get; set; }
+
+    public Dresseur(float posX, float posY, int idMap, string name)
+        : base(posX, posY, 1, idMap)
+    {
+        Name = name;
+        ChakimonEquipe = new List<Chakimon>();
+    }
+}
